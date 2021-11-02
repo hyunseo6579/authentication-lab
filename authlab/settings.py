923d54ab4ebd74fe295e5c8e5c4e4cedbac3690d
hyunseo6579 <hyunseo6579>
2021-11-01 21:29:51 -0600

    lab9

--- authlab/settings.py
+++ authlab/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
+    'rest_framework.authtoken',
     'client.apps.ClientConfig',
 ]
 
@@ -128,5 +130,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     )
 }
